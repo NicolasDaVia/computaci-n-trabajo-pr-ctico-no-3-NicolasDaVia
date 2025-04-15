8039032a7257acfaaa01aab8a6720241d6afde05
NicolasDaVia <n.davia@alumno.um.edu.ar>
2025-04-15 22:09:41 +0000

    Agregar tests para ingreso de numeros negativos

--- src/test_calculo_numeros.py
+++ src/test_calculo_numeros.py
@@ -5,8 +5,11 @@ from excepcion import NegativeNumberError
 
 class TestIngresoNumero(unittest.TestCase):
 
-    @patch('builtins.input', return_value='100')
-    def test_numero_valido(self, mocked_input):
-        resultado = ingrese_numero()
-        self.assertEqual(resultado, 100.0)
-### 
+
+     @patch('builtins.input', return_value='-50')
+    def test_numero_negativo(self, mocked_input):
+        with self.assertRaises(NegativeNumberError):
+            ingrese_numero()
+
+if __name__ == '__main__':
+    unittest.main()
