59f74731751c9e3069824e18335966ea77e4eb65
NicolasDaVia <n.davia@alumno.um.edu.ar>
2025-04-15 22:56:34 +0000

    Codigo completo

--- src/test_calculo_numeros.py
+++ src/test_calculo_numeros.py
@@ -5,10 +5,21 @@ from excepcion import NegativeNumberError
 
 class TestIngresoNumero(unittest.TestCase):
 
-   @patch('builtins.input', return_value='ABC')
+    @patch('builtins.input', return_value='100')
+    def test_numero_valido(self, mocked_input):
+        resultado = ingrese_numero()
+        self.assertEqual(resultado, 100.0)
+
+    @patch('builtins.input', return_value='-50')
+    def test_numero_negativo(self, mocked_input):
+        with self.assertRaises(NegativeNumberError):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='ABC')
     def test_entrada_no_numerica(self, mocked_input):
         with self.assertRaises(ValueError):
             ingrese_numero()
 
 if __name__ == '__main__':
     unittest.main()
+
